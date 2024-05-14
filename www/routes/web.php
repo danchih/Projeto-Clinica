df20d984d5a5e7092832a586f1646db7be438573
flaviamedeiross <flaviacris25@icloud.com>
2024-05-14 14:33:55 -0300

    Landpage

--- www/routes/web.php
+++ www/routes/web.php
@@ -28,4 +28,9 @@ Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/login', function () {
+    return Inertia::render('Login');
+})->name('login');
+
 require __DIR__.'/auth.php';
+
