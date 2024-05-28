409edf04bd41aa719ec5305c30dd205e3062f504
danchih <dani.hayashi88@hotmail.com>
2024-05-28 12:15:58 -0300

    testes_atualizados

--- www/tests/Feature/Auth/AuthenticationTest.php
+++ www/tests/Feature/Auth/AuthenticationTest.php
@@ -11,35 +11,35 @@ class AuthenticationTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_login_screen_can_be_rendered()
-    {
-        $response = $this->get('/login');
-
-        $response->assertStatus(200);
-    }
-
     public function test_users_can_authenticate_using_the_login_screen()
     {
-        $user = User::factory()->create();
-
-        $response = $this->post('/login', [
-            'email' => $user->email,
-            'password' => 'password',
+        // Crie um usuário com uma senha válida
+        $user = User::factory()->create([
+            'password' => bcrypt('password'),
         ]);
 
-        $this->assertAuthenticated();
+        $response = $this->post('/login', [
+            'username' => $user->username,
+            'password' => 'password', // Use a mesma senha que foi definida no usuário
+        ]);
+
+        $this->assertAuthenticated(); // Verifica se o usuário está autenticado
         $response->assertRedirect(RouteServiceProvider::HOME);
     }
 
     public function test_users_can_not_authenticate_with_invalid_password()
     {
-        $user = User::factory()->create();
+        // Crie um usuário com uma senha válida
+        $user = User::factory()->create([
+            'password' => bcrypt('password'),
+        ]);
 
+        // Tente autenticar com uma senha inválida
         $this->post('/login', [
-            'email' => $user->email,
+            'username' => $user->username,
             'password' => 'wrong-password',
         ]);
 
-        $this->assertGuest();
+        $this->assertGuest(); // Verifica se o usuário não está autenticado
     }
 }
